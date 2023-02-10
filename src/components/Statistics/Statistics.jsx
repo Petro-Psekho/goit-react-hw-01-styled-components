fd0f16eec3826acfc0217134a9569341e2eadc82
Petro Psekho <psekho.pp@gmail.com>
2023-02-10 09:07:28 +0200

    refactoring stiled components Statistics

--- src/components/Statistics/Statistics.jsx
+++ src/components/Statistics/Statistics.jsx
@@ -1,25 +1,25 @@
 import PropTypes from 'prop-types';
-import { getRandomColor } from 'components/utils/getRandomColor';
-import css from 'components/Statistics/Statistics.module.css';
-import {Section, StatTitle, StatList} from 'components/Statistics/Statistics.styled'
+
+import {
+  Section,
+  StatTitle,
+  StatList,
+  StatListItem,
+  StatListLabel,
+  StatListPercentage,
+} from 'components/Statistics/Statistics.styled';
 
 export const Statistics = ({ title, stats }) => {
   return (
     <Section>
       <StatTitle>{title}</StatTitle>
 
-      <StatList className={css.statList}>
+      <StatList>
         {stats.map(stat => (
-          <li
-            key={stat.id}
-            className={css.item}
-            style={{
-              backgroundColor: getRandomColor(),
-            }}
-          >
-            <span className={css.label}>{stat.label}</span>
-            <span className={css.percentage}>{stat.percentage}</span>
-          </li>
+          <StatListItem key={stat.id} bgColor={stats}>
+            <StatListLabel>{stat.label}</StatListLabel>
+            <StatListPercentage>{stat.percentage}</StatListPercentage>
+          </StatListItem>
         ))}
       </StatList>
     </Section>
@@ -32,6 +32,6 @@ Statistics.propTypes = {
       id: PropTypes.string.isRequired,
       label: PropTypes.string.isRequired,
       percentage: PropTypes.number.isRequired,
-    }),
+    })
   ),
 };
--- src/components/Statistics/Statistics.styled.jsx
+++ src/components/Statistics/Statistics.styled.jsx
@@ -1,4 +1,5 @@
 import styled from '@emotion/styled';
+import { getRandomColor } from 'components/utils/getRandomColor';
 
 export const Section = styled.section`
   text-align: center;
@@ -6,16 +7,36 @@ export const Section = styled.section`
   margin: 0 auto 50px;
   border-radius: 4px;
   overflow: hidden;
-  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.2);  
-`
+  box-shadow: 0 2px 5px rgba(0, 0, 0, 0.2), 0 4px 6px rgba(0, 0, 0, 0.2);
+`;
 
 export const StatTitle = styled.h2`
   text-transform: uppercase;
   padding: 40px 0;
   color: #2e4054;
-`
+`;
 
 export const StatList = styled.ul`
   display: flex;
   justify-content: center;
-`
+`;
+
+export const StatListItem = styled.li`
+  width: 100px;
+  padding: 10px 0;
+  display: flex;
+  flex-direction: column;
+  background-color: ${props => getRandomColor()};
+`;
+
+export const StatListLabel = styled.span`
+  font-weight: 400;
+  text-transform: lowercase;
+  color: #f5f5f5;
+`;
+
+export const StatListPercentage = styled.span`
+  font-size: 30px;
+  font-weight: 600;
+  color: #f5f5f5;
+`;
